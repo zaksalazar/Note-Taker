48b4e6596d5137d56b0d04db080d5b675363d20d
Zak Salazar <therangerzakshow@Thes-MacBook-Pro.local>
2022-12-10 14:13:48 -0800

    fixed server bug, app working properly

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const path = require('path');
-const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require("./routes /apiRoutes");
 const app = express();
 
 //middleware
